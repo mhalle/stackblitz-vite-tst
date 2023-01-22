bc916d81150aed8b59d019f7e3a531bc905e4b0e
mhalle <m@halle.us>
2023-01-22 10:56:34 -0500

    hooks test code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import { useState } from 'react';
+import { Button, DatePicker, Space, version } from 'antd';
+import 'antd/dist/reset.css';
 import './App.css';
 
 function App() {
-  return <div className="App">wow</div>;
+  const [count, setCount] = useState(0);
+
+  const incrCount = () => {
+    setCount(count + 1);
+  };
+
+  return <Controls count={count} incr={incrCount} />;
 }
+
+const Controls = (props: any) => {
+  return (
+    <div className="App">
+      <Space>
+        <DatePicker />
+        <span>{props.count}</span>
+        <Button type="primary" onClick={props.incr}>
+          Primary Button
+        </Button>
+      </Space>
+    </div>
+  );
+};
 
 export default App;
